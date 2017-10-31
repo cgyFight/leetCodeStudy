821490b00d4368be64069ef6f4254e93e2a7f071
cgy <at_cgy@qq.com>
2017-10-31 19:30:09 +0800

    20171031合并K个排序的队列

--- testMain.cpp
+++ testMain.cpp
@@ -4,39 +4,113 @@
 using namespace std;
 
 struct ListNode {
-  int val;
-  ListNode *next;
+  ListNode(int x, char z) : val(x), c(z), next(NULL) {}
   ListNode(int x) : val(x), next(NULL) {}
+  int val;
+  char c;
+  ListNode *next;
 };
  
+ #if 1
 class Solution {
 public:
+	void func(){
+
+	}
+	/*遍历每一个队列，找出其中最小的，加入到最新的*/
     ListNode* mergeKLists(vector<ListNode*>& lists) {
     	ListNode newListDummy(0);
     	ListNode *next = &newListDummy;
     	ListNode *pSmallstNode;
-    	int smalstVal = pSmallstNode->val;
-
-    	if (lists.isempty()) return NULL;
-
-    	pSmallstNode = lists[0];
-    	while(pSmallstNode) {
-    		for (std::vector<>::iterator i = lists.begin(); i != lists.end(); ++i) {
-    			if (*i == NULL) continue;//end of an list
+    	std::vector<ListNode *>::iterator tmp;
+    	while(next) {
+    		pSmallstNode = NULL;
+    		for (std::vector<ListNode*>::iterator i = lists.begin(); i != lists.end(); ++i) {
+    			if (*i == NULL) continue;
+    			if (!pSmallstNode) pSmallstNode = *i;
     			if ((*i)->val <= pSmallstNode->val) {
     				pSmallstNode = *i;
-    				*i = (*i)->next;
-    				next->next = pSmallstNode;
-    				next = next->next;
-    			} 
+    				tmp = i;
+    			}
+    		}
+    		if (pSmallstNode) *tmp = (*tmp)->next;
+    		next->next = pSmallstNode;
+    		next = next->next;
+    	}
+    	return newListDummy.next;
+    }
+
+    /*合并两个队列*/
+    ListNode *mergeTwoList(ListNode *l1, ListNode *l2) {
+    	ListNode newListDummy(0);
+    	ListNode *next = &newListDummy;
+    	while(true) {
+    		if (!l1) {
+    			next->next = l2;
+    			break;
+    		}
+    		if (!l2) {
+    			next->next = l1;
+    			break;
     		}
     	}
     }
+
+    /*合并两个队列*/
+    ListNode *mergeTwoList2(ListNode *l1, ListNode *l2) {
+    	if (!l1) return l2;
+    	if (!l2) return l1;
+    	ListNode newListDummy(0);
+    	ListNode *next = &newListDummy;
+    	next->next = l1;
+    	ListNode *tmp;
+
+    	while (l1 && l2) {
+    		if (l1->val < l2->val) {
+    			l1 = l1->next;
+    		} else {
+    			tmp = l2;
+    			l2 = l2->next;
+    			tmp->next = l1;
+
+    		}
+    		next = next->next;
+    	}
+
+
+    	while(next) {
+    		if (!l1) {
+				next->next = l2;
+				break;
+    		} 
+    		if (!l2) {
+    			next->next = l1;
+    			break;
+    		}
+
+    		if (l1->val < l2->val) {
+    			next->next = l1;
+    			l1 = l1->next;
+    		} else {
+    			next->next = l2;
+    			l2 = l2->next;
+    		}
+			next = next->next;
+    	}
+    }
+
+    /*
+    	两个两个的排序，再并入下一个
+     */
+    ListNode *mergeKLists2(std::vector<ListNode *> &lists){
+    }
 };
 
+#endif
 
 int main(int argc, char const *argv[])
 {
+#if 1
 	/* code */
 	struct ListNode a1(1, 'a');
 	struct ListNode a2(2, 'a');
@@ -65,11 +139,17 @@ int main(int argc, char const *argv[])
 
 	std::vector<ListNode *>::iterator iter;
 	iter = v.begin();
-	tmp = (*iter);
-	(*iter) = (*iter)->next;
+
+	cout << "befor size " << v.size();
+	v.erase(iter);
+	cout << "end size " << v.size();
+
+	// tmp = (*iter);
+	// (*iter) = (*iter)->next;
 
 	// cout <<  "hello" << v[0]->val << v[0]->type;
 	// cout << "tmp val = " << tmp->val;
+	#endif
 
 	return 0;
 }
